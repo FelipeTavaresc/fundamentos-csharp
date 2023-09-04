254eeb8b759a4f747bdd2be7534720048c8da880
José Felipe Tavares <jose.costa@csgd.com.br>
2023-09-04 09:55:26 -0300

    Inicializando o projeto

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("First value: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine(v1);
+        }
+    }
+}
