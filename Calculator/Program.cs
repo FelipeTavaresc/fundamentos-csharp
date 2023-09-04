e571a64f4db18060aa3555a240466dbd60eea044
Felipe <felipe.tavares@outlook.com>
2023-09-04 11:15:13 -0300

    Call functions

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -21,11 +21,20 @@ namespace Calculator
             Console.WriteLine("---------------------------------");
             Console.Write("Select one option: ");
             short result = short.Parse(Console.ReadLine());
+            switch (result)
+            {
+                case 1: Sum(); break;
+                case 2: Subtraction(); break;
+                case 3: Division(); break;
+                case 4: Multiplication(); break;
+                default: Menu(); break;
+            }
         }
 
         static void Sum()
         {
             Console.Clear();
+            Console.WriteLine("** Sum Operation ***");
             Console.Write("First value: ");
             float v1 = float.Parse(Console.ReadLine());
 
@@ -37,11 +46,13 @@ namespace Calculator
             float result = v1 + v2;
             Console.WriteLine($"The result of sum is: {result}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Subtraction()
         {
             Console.Clear();
+            Console.WriteLine("** Subtraction Operation ***");
             Console.Write("First value: ");
             float v1 = float.Parse(Console.ReadLine());
 
@@ -53,11 +64,13 @@ namespace Calculator
             float result = v1 - v2;
             Console.WriteLine($"The result of subtraction is: {result}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Division()
         {
             Console.Clear();
+            Console.WriteLine("** Division Operation ***");
             Console.Write("First value: ");
             float v1 = float.Parse(Console.ReadLine());
 
@@ -69,11 +82,13 @@ namespace Calculator
             float result = v1 / v2;
             Console.WriteLine($"The result of division is: {result}");
             Console.ReadKey();
+            Menu();
         }
 
         static void Multiplication()
         {
             Console.Clear();
+            Console.WriteLine("** Multiplication Operation ***");
             Console.Write("First value: ");
             float v1 = float.Parse(Console.ReadLine());
 
@@ -85,6 +100,7 @@ namespace Calculator
             float result = v1 * v2;
             Console.WriteLine($"The result of multiplication is: {result}");
             Console.ReadKey();
+            Menu();
         }
     }
 }
