3cdf74da58e5bde95f99ff171064d5e2fdf525c7
Felipe <felipe.tavares@outlook.com>
2023-09-05 19:43:21 -0300

    Add prestart

--- Stopwatch/Program.cs
+++ Stopwatch/Program.cs
@@ -28,8 +28,21 @@ namespace Stopwatch
             if (time == 0)
                 Environment.Exit(0);
 
-            Start(time * multiplier);
+            PreStart(time * multiplier);
         }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("Ready...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go...");
+            Thread.Sleep(2000);
+            Start(time);
+        }
+
         static void Start(int time)
         {
             int currentTime = 0;
