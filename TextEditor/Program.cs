59d9631c15db41f4820211b2d6c0d2ad602c2d06
Felipe <felipe.tavares@outlook.com>
2023-09-06 21:40:21 -0300

    Init project

---
+++ TextEditor/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("What you would like to do?");
+            Console.WriteLine("1 - Open file");
+            Console.WriteLine("2 - Create new file");
+            Console.WriteLine("0 - Exit");
+            short option = short.Parse(Console.ReadLine());
+            switch (option)
+            {
+                case 0: Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Close(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Open()
+        {
+
+        }
+
+        static void Close()
+        {
+
+        }
+    }
+}
