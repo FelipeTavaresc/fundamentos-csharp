a4f6f6871d7647a53b197604f9e8e53c1b88d3ac
Felipe <felipe.tavares@outlook.com>
2023-09-13 20:23:10 -0300

    Showing menu

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -28,6 +28,26 @@ namespace EditorHtml
             }
 
             DrawHeadAndFooterLine();
+            WriteOptions();
+            var option = short.Parse(Console.ReadLine());
+        }
+
+        public static void WriteOptions()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("-------------");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir Arquivo");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 9);
+            Console.Write("Opção: ");
         }
 
         public static void DrawHeadAndFooterLine()
