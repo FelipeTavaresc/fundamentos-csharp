be030369322145d96ba9ab5f5132e2e2e01285b3
Felipe <felipe.tavares@outlook.com>
2023-09-13 20:11:53 -0300

    Drawing screen

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -9,6 +9,35 @@ namespace EditorHtml
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.Blue;
             Console.ForegroundColor = ConsoleColor.Black;
+
+            DrawScreen();
+        }
+
+        public static void DrawScreen()
+        {
+            DrawHeadAndFooterLine();
+
+            for (int lines = 0; lines < 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+                Console.Write("|");
+                Console.Write("\n");
+            }
+
+            DrawHeadAndFooterLine();
+        }
+
+        public static void DrawHeadAndFooterLine()
+        {
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.Write("+");
+            Console.Write("\n");
         }
     }
 }
